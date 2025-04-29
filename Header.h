4bfd339b3df8e21de65fe46f9f16f3874e717f31
xxynesahan <163707673+xxynesahan@users.noreply.github.com>
2025-04-30 02:40:00 +0700

    Update Header.h

--- Header.h
+++ Header.h
@@ -1,20 +1,54 @@
 #include <iostream>
 #include <vector>
+#include <string>
+#include <ctime>
+#include <fstream>
+#include <sstream>
 #include <Windows.h>
 
 using namespace std;
 
-struct User { string username, password, role;};
-struct Test { string name, description, role;};
+struct User {
+    string username;
+    string password;
+    string role;
+    bool hasVaultAccess;
+};
 
-class DataBase {
-    vector<User> users;
-    vector<Test> tests;;
+struct AccessAttempt {
+    string username;
+    string timestamp;
+    bool successful;
+};
+
+class SimpleDB {
+    const string USERS_FILE = "users.db";
+    const string LOGS_FILE = "access_logs.db";
 
 public:
-    DataBase();
-    bool addUser(string username, string password, string role);
-    string getRole(string username, string password);
-    vector<Test> getTest(string role);
-    bool removeUser(string username);
+    vector<User> loadUsers();
+    bool saveUsers(const vector<User>& users);
+    vector<AccessAttempt> loadAccessLogs();
+    bool saveAccessLog(const AccessAttempt& log);
 };
+
+class VaultDatabase {
+    SimpleDB db;
+    vector<User> users;
+    vector<AccessAttempt> accessLogs;
+
+    void loadData();
+
+public:
+    VaultDatabase();
+    bool addUser(string username, string password, string role, bool hasAccess);
+    string authenticate(string username, string password);
+    bool attemptVaultAccess(string username);
+    void logAccessAttempt(string username, bool success);
+    void showAccessLogs();
+    vector<User> getUsersWithAccess();
+    bool removeUser(const string& username);
+};
+
+void adminMenu(VaultDatabase& db);
+void userMenu(VaultDatabase& db, string username);
